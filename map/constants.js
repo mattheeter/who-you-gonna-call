59f0923dd175478129471b27b3c24de1d582473d
austinheiss <33814164+austinheiss@users.noreply.github.com>
2026-03-30 21:59:58 -0400

    recenter the map for new layout

--- map/constants.js
+++ map/constants.js
@@ -18,7 +18,7 @@ export const DATA_URL = encodeURI(
   "./data/Cincinnati_311_(Non-Emergency)_Service_Requests_20260227.csv"
 );
 export const BOUNDARIES_GEOJSON_URL = "./data/cincinnati.geojson";
-export const CINCINNATI_CENTER = [39.143, -84.53];
+export const CINCINNATI_CENTER = [39.137, -84.5];
 export const DEFAULT_ZOOM = 12;
 export const MISSING_VALUE_COLOR = "#94a3b8";
 export const UNKNOWN_CATEGORY_COLOR = MISSING_VALUE_COLOR;
